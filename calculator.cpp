902fb7cea099e487073d29e5c00fb59fddd34116
LeonidLozko <lozkoll@gmail.com>
2019-10-20 20:38:43 +0300

    commit with adding comments

--- calculator.cpp
+++ calculator.cpp
@@ -1,15 +1,21 @@
 #include "calculator.h"
 
+# adding operation of two float numbers 
+# with fixing truncation error
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
 }
 
+# subtraction operation of two float numbers 
+# using function Add with fixed truncation error
 int Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
 }
 
+# multiplication operation of two float numbers 
+# with fixing truncation error
 int Calculator::Mul (double a, double b)
 {
 	return a * b + 0.5;
